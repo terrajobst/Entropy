b2fb7a84daa335eff26c987590dd706df2fc261c
Hao Kung <haok@microsoft.com>
2017-09-11 12:09:20 -0700

    Add Cookie auth sample

---
+++ samples/Authentication.Cookies/ClaimsTransformer.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Authentication.Cookies
+{
+    public class ClaimsTransformer : IClaimsTransformation
+    {
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            // This will run every time Authenticate is called.
+            principal.Identities.First().AddClaim(new Claim("Hello", "ClaimsTransform"));
+            return Task.FromResult(principal);
+        }
+    }
+}
---
+++ samples/Authentication.Cookies/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Authentication.Cookies.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
